2af5707440a5b32d6ecef9f08769eef816aeb483
Dan Shirley <dan@shopthru.com>
2025-09-17 21:26:30 +0100

    renamed app dir to cli

--- app/dist/types/core.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * Core data models for Food Truck Manager game
- * Based on design documentation at design_docs/food-truck-manager-design/
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=core.js.map
